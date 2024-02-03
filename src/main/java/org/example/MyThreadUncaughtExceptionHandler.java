5b5ca0b404749f33425ce29ac29c874c8651ed44
aleksLis <darklisik.darklisik@yandex.ru>
2024-02-04 01:01:36 +0300

    handling RuntimeException

--- src/main/java/org/example/MyThreadUncaughtExceptionHandler.java
+++ src/main/java/org/example/MyThreadUncaughtExceptionHandler.java
@@ -1,2 +1,28 @@
-package org.example;public class MyThreadUncaughtExceptionHandler {
+package org.example;
+
+import static java.lang.Thread.*;
+
+public class MyThreadUncaughtExceptionHandler {
+
+    public static void main(String[] args) {
+
+        Thread thread1 = new Thread(() -> {
+            throw new RuntimeException("Message from thread");
+        });
+
+
+        UncaughtExceptionHandler uncaughtExceptionHandler = (thread, exception) -> {
+            System.out.printf("Exception: '%s' from thread: '%s'\n", exception.getMessage(), thread.getName());
+        };
+
+        //Method 1
+        Thread.setDefaultUncaughtExceptionHandler(uncaughtExceptionHandler);
+
+        //Method 2
+        thread1.setUncaughtExceptionHandler(uncaughtExceptionHandler);
+
+        thread1.start();
+
+
+    }
 }
