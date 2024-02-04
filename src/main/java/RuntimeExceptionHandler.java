22aede51ac31740ee92eac57d84e348fc5c9a73a
aleksLis <darklisik.darklisik@yandex.ru>
2024-02-04 20:23:49 +0300

    Runtimeexception handler

---
+++ src/main/java/RuntimeExceptionHandler.java
@@ -0,0 +1,27 @@
+public class RuntimeExceptionHandler implements Runnable{
+
+    public static void main(String[] args) {
+
+        RuntimeExceptionHandler runtimeExceptionHandler = new RuntimeExceptionHandler();
+        Thread thread1 = new Thread(runtimeExceptionHandler);
+
+
+        Thread.UncaughtExceptionHandler uncaughtExceptionHandler = (thread, ex) -> {
+            System.out.printf("Error: '%s' from thread '%s'.\n", ex.getMessage(), thread.getName());
+        };
+
+        //Method 1
+        Thread.setDefaultUncaughtExceptionHandler(uncaughtExceptionHandler);
+
+        //Method 2
+        thread1.setUncaughtExceptionHandler(uncaughtExceptionHandler);
+
+
+        thread1.start();
+    }
+
+    @Override
+    public void run() {
+        throw new RuntimeException("Message from method run");
+    }
+}
